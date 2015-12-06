90eeb9a99cbb3909efb08e3353e63cd298a72b53
박종복 <okps123@naver.com>
2015-12-06 09:42:20 +0900

    BorderlessWindow 추가

---
+++ RayeUI/Control/BorderlessWindow.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace RayeUI.Control
+{
+    public class BorderlessWindow : Window
+    {
+        public readonly static DependencyProperty CaptionHeightProperty = DependencyProperty.Register("CaptionHeight", typeof(int), typeof(BorderlessWindow), new PropertyMetadata(0));
+        public int CaptionHeight
+        {
+            get
+            {
+                return (int)GetValue(CaptionHeightProperty);
+            }
+            set
+            {
+                SetValue(CaptionHeightProperty, value);
+            }
+        }
+
+        public readonly static DependencyProperty ResizeBorderThicknessProperty = DependencyProperty.Register("ResizeBorderThickness", typeof(Thickness), typeof(BorderlessWindow), new PropertyMetadata(new Thickness(5)));
+        public Thickness ResizeBorderThickness
+        {
+            get
+            {
+                return (Thickness)GetValue(ResizeBorderThicknessProperty);
+            }
+            set
+            {
+                SetValue(ResizeBorderThicknessProperty, value);
+            }
+        }
+
+        public readonly static DependencyProperty GlassFrameThicknessProperty = DependencyProperty.Register("GlassFrameThickness", typeof(Thickness), typeof(BorderlessWindow), new PropertyMetadata(new Thickness(0,0,0,1)));
+        public Thickness GlassFrameThickness
+        {
+            get
+            {
+                return (Thickness)GetValue(GlassFrameThicknessProperty);
+            }
+            set
+            {
+                SetValue(GlassFrameThicknessProperty, value);
+            }
+        }
+
+        public BorderlessWindow() :base()
+        {
+            base.Style = (Style)FindResource("BorderlessWindowStyle");
+        }
+    }
+}
